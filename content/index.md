40e0f41d3652c9b49afdbf318b93b3adfcbedf7d
Drew Bornt <andrew.bornt@gmail.com>
2024-07-01 14:04:08 -0500

    Quartz sync: Jul 1, 2024, 2:04 PM

--- content/index.md
+++ content/index.md
@@ -4,3 +4,4 @@ title: Homepage
 
 Welcome to my public repository for all things IT, Cybersecurity, and Penetration Testing. I will have write-ups for various CTFs, HackTheBox rooms, TryHackMe rooms, and more.
 
+You can find my 
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Drew's Ethical Hacking Repo",
+    pageTitle: "Drew's Mind Palace",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
