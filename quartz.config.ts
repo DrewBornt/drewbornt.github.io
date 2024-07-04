a0372846f08a913e0d7fe82ad730b6998b150235
Drew Bornt <andrew.bornt@gmail.com>
2024-07-04 08:22:03 -0500

    Quartz sync: Jul 4, 2024, 8:22 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "drewbornt.github.io",
+    baseUrl: "drewscyberops.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
